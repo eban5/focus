f1eec920ecdf625d1d371ab2ef0df5a4f4e7181b
Esteban Amas <estebanamas@gmail.com>
2022-04-01 18:42:00 -0400

    console log allowed

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
