6466e19e35eb687f686939b52195d79c28104467
Esteban Amas <estebanamas@gmail.com>
2022-04-05 22:04:54 -0400

    Remove unused

--- src/renderer/Progress.tsx
+++ src/renderer/Progress.tsx
@@ -1,12 +1,4 @@
-export const toTitleCase = (value: string): string => {
-  // split by whitespace
-  // capitalize first char of each word
-  // join and return
-  return value
-    .split(' ')
-    .map((word: string) => word.charAt(0).toUpperCase() + word.substring(1))
-    .join(' ');
-};
+import { toTitleCase } from './format';
 
 const Progress = (props: {
   intervals: string[];
